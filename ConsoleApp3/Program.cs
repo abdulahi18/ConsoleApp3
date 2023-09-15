239000db71d2f87bef04594568ef8a747a894453
abib05001 <abib05001@E-PW0234M2>
2023-09-15 12:41:51 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vad är din favoritbok?");
+            string favoritbok = Console.ReadLine();
+            
+            Console.WriteLine("Din favorit bok är " +favoritbok+" Det är en bra bok");
+        }
+        
+    }
+}
